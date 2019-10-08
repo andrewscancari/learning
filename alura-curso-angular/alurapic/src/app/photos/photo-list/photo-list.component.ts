48124678feb452b812e1ecfb20f10d6d7a43b495
Andrew Scancari <andrew.scancari@compasso.com.br>
2019-10-08 14:15:07 -0300

    Atualização com a finalização da parte 1 do curso de Angular da Alura

--- alura-curso-angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alura-curso-angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -6,35 +6,32 @@ import { debounceTime } from "rxjs/operators";
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
-    selector: 'app-ap-photo-list',
-    templateUrl: './photo-list.component.html',
-    styleUrls: ['./photo-list.component.css']
+  selector: 'app-ap-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
-    photos: Photo[] = [];
-    filter: string = '';
-    debounce: Subject<string> = new Subject<string>();
-    hasMore: boolean = true;
-    currentPage: number = 1;
-    userName: string = '';
+export class PhotoListComponent implements OnInit {
+  photos: Photo[] = [];
+  filter: string = '';
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
-    constructor(private activatedRoute: ActivatedRoute, private photoService: PhotoService) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) {}
 
-    ngOnInit(): void {
-        this.userName = this.activatedRoute.snapshot.params.userName;
-        this.photos = this.activatedRoute.snapshot.data['photos'];
+  ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+  }
 
-        this.debounce.pipe(debounceTime(300)).subscribe(filter => this.filter = filter);
-    }
-
-    ngOnDestroy(): void {
-        this.debounce.unsubscribe();
-    }
-
-    load() {
-        this.photoService.listFromUserPaginated(this.userName, ++this.currentPage).subscribe(photos => {
-            this.photos = this.photos.concat(photos);
-            if(!photos.length) this.hasMore = false;
-        })
-    }
+  load() {
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage).subscribe(photos => {
+      this.filter = '';
+      this.photos = this.photos.concat(photos);
+      if(!photos.length) this.hasMore = false;
+    })
+  }
 }
